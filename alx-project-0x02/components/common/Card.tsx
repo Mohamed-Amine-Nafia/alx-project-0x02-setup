c5f6a3e3811d418e58debd2f6d7b90963766614f
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-07-25 14:43:21 +0200

    fix the code

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { CardProps } from "@/interfaces";
 
-const Card: React.FC<CardProps> = ({ title, content }) => {
+const Card = ({ title, content }: CardProps) => {
   return (
-    <div className="bg-white rounded-xl shadow-md p-6 border border-gray-200 hover:shadow-lg transition">
-      <h3 className="text-xl font-semibold mb-2">{title}</h3>
-      <p className="text-gray-700">{content}</p>
+    <div>
+      <h3>{title}</h3>
+      <p>{content}</p>
     </div>
   );
 };
